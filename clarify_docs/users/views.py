33e011dd3d6d6582afced241e6478f9cc70788b0
Artur Mistiuk <mistiuk.artur@gmail.com>
2023-11-10 01:38:41 +0100

    Create views.py

---
+++ clarify_docs/users/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render, redirect
+
+from .forms import SignupForm
+
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('signup')
+    else:
+        form = SignupForm()
+
+    return render(request, 'users/signup.html', {'form': form})
