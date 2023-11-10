c236960b8c19e3d2770c491d792e83e4a4aeebc0
Artur Mistiuk <mistiuk.artur@gmail.com>
2023-11-10 02:01:24 +0100

    Update forms.py

--- clarify_docs/users/forms.py
+++ clarify_docs/users/forms.py
@@ -7,21 +7,21 @@ from django.contrib.auth.models import User
 class SignupForm(UserCreationForm):
     username = forms.CharField(
         max_length=35,
-        help_text="Required. 35 characters or fewer. <br>Letters, digits and @/./+/-/_ only."
+        help_text="Required. 35 characters or fewer. <br>Letters, digits and @/./+/-/_ only.",
     )
 
     email = models.EmailField(
-        help_text="Required. Enter a valid email address.",
-        unique=True
+        help_text="Required. Enter a valid email address.", unique=True
     )
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ["username", "email", "password1", "password2"]
 
     def clean_email(self):
-        email = self.cleaned_data.get('email')
+        email = self.cleaned_data.get("email")
         if User.objects.filter(email=email).exists():
             raise forms.ValidationError(
-                'This email address is already in use. Please provide a different email address.')
+                "This email address is already in use. Please provide a different email address."
+            )
         return email
