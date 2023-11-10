0bac142c77ba4f236ee49a0e166eef095603c7f1
Artur Mistiuk <mistiuk.artur@gmail.com>
2023-11-10 03:19:59 +0100

    Update views.py

--- clarify_docs/users/views.py
+++ clarify_docs/users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render, redirect
 
-from .forms import SignupForm
+from .forms import SignupForm, LoginForm
 
 
 def signup_view(request):
@@ -10,8 +10,19 @@ def signup_view(request):
         form = SignupForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect("signup")
+            return redirect("login")
     else:
         form = SignupForm()
 
     return render(request, "users/signup.html", {"form": form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            return redirect("login")
+    else:
+        form = LoginForm()
+
+    return render(request, "users/login.html", {"form": form})
