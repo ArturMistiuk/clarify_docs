71e3ff2cd27753e2bc843736bbd652fb4f307ed2
Artur Mistiuk <mistiuk.artur@gmail.com>
2023-11-11 00:44:26 +0100

    Update views.py
    Changed docstrings for views functions

--- clarify_docs/users/views.py
+++ clarify_docs/users/views.py
@@ -1,20 +1,21 @@
 from django.http import HttpResponseRedirect
 from django.http import HttpRequest
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 from .forms import SignupForm, LoginForm
 
 
 def signup_view(request: HttpRequest) -> HttpResponseRedirect:
     """
-The signup_view function is a view that handles the signup form.
-It takes in a request and returns an HTML page with the signup form.
-If the user submits valid data, it creates a new user account and redirects to login.
+The signup_view function handles the signup process for a new user.
+It takes an HttpRequest object as its first argument, and returns an HttpResponseRedirect object that redirects to the login page if successful.
+If unsuccessful, it renders the users/signup.html template with a SignupForm instance in its context.
 
-:param request: Get the data from the form
-:return: A rendered template
-    """
-
+:param request: HttpRequest: Pass the request to the view
+:return: An httpresponseredirect object, which redirects the user to the login page
+"""
     if request.method == "POST":
         form = SignupForm(request.POST)
         if form.is_valid():
@@ -28,18 +29,21 @@ If the user submits valid data, it creates a new user account and redirects to l
 
 def login_view(request: HttpRequest) -> HttpResponseRedirect:
     """
-    The login_view function is responsible for handling the login process.
-    It will display a form to the user, and if that form is valid, it will log in
-    the user and redirect them to their profile page.
+The login_view function is responsible for handling the login process.
+It will first check if the request method is POST, and if so it will attempt to authenticate a user with the given
+username and password. If authentication fails, an error message will be displayed on screen. Otherwise, we log in
+the user using Django's built-in login function.
 
-    :param request: Get the request data from the user
-    :return: An http response object that renders the login
-    """
+:param request: HttpRequest: Pass the request object to the view
+:return: A httpresponseredirect object, which is a subclass of the httpresponse class
+"""
     if request.method == "POST":
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            return redirect("login")
-    else:
-        form = LoginForm()
+        user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            messages.error(request, 'Username or password didn\'t match')
+            return redirect(to='login')
 
-    return render(request, "users/login.html", {"form": form})
+        login(request, user)
+        return redirect(to='signup')
+
+    return render(request, 'users/login.html', context={"form": LoginForm()})
